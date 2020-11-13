87db9cb6c5ef1cfcb4bb2bf2e979e1b94e525b17
suna001 <nooneed@icloud.com>
2020-11-13 22:18:17 +0900

    モデルとテーブルの作成+ルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,10 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
