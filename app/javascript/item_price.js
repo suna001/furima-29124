93951a7667b2392d7a686762a73ebd026209fb45
suna001 <nooneed@icloud.com>
2020-11-15 10:50:33 +0900

    販売価格設定

---
+++ app/javascript/item_price.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+   const inputValue = priceInput.value;
+   const addTaxDom = document.getElementById('add-tax-price');
+   const taxValue = Math.floor(inputValue*0.10);
+    
+    addTaxDom.innerHTML = taxValue
+      const salesProfit = document.getElementById('profit');
+      salesProfit.innerHTML = Math.floor(inputValue - taxValue);
+
+    console.log(taxValue);
+  })
+
+
+});
