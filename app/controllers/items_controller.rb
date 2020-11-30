c1b3592118ecd43f84f9a8e5504837302ff46e6e
suna001 <nooneed@icloud.com>
2020-11-30 18:42:07 +0900

    商品詳細表示機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,18 +23,18 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-  end
+  # def edit
+  # end
   
-  def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-  end
+  # def update
+  #   item = Item.find(params[:id])
+  #   item.update(item_params)
+  # end
 
-  def destroy
-    item = Item.find(params[:id])
-    item.destroy
-  end
+  # def destroy
+  #   item = Item.find(params[:id])
+  #   item.destroy
+  # end
 
   private
 
